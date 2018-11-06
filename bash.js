f2b492c354ffdd00fd8608910a60f831865583ac
k-westbrook <kcheriewestbrook@gmail.com>
2018-11-06 15:32:52 -0500

    Bonus changes

--- bash.js
+++ bash.js
@@ -2,15 +2,20 @@ process.stdout.write("prompt > ");
 
 const ls = require('./ls')
 const pwd = require('./pwd')
+const cat = require('./cat')
 process.stdin.on("data", data => {
   const cmd = data.toString().trim();
   if (cmd === "pwd") {
     pwd();
   }
-  else if(cmd === "ls") {
-   ls();
+  else if (cmd === "ls") {
+    ls();
   }
-   else {
+  else if (cmd.split(' ')[0] === 'cat') {
+
+    cat(cmd.split(' ')[1]);
+
+  } else {
     process.stdout.write("You typed: " + cmd);
   }
   process.stdout.write("\nprompt > ");
